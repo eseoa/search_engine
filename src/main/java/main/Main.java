8cfff816218d061d7564752bcad2e68858a7d595
Arkady Railyanu <eseo.a@yandex.ru>
2022-06-04 13:18:53 +0300

    remove test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         HibernateUtil.getHibernateSession().close();
         SpringApplication.run(Main.class, args);
-        System.out.println();
 
     }
 }
