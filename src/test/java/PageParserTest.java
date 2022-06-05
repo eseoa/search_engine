c9ad94505d3f594daacd1a8964326bdb85a48c08
Arkady Railyanu <eseo.a@yandex.ru>
2022-06-05 17:44:12 +0300

    comment tests

--- src/test/java/PageParserTest.java
+++ src/test/java/PageParserTest.java
@@ -14,36 +14,36 @@ import java.util.Optional;
 
 public class PageParserTest {
 
-    private String path = "https://skillbox.ru/";
-    private String siteName = "skillbox";
-
-    @Test
-    @DisplayName("тест парсинга страницы")
-    public void parse () {
-        Session session = HibernateUtil.getHibernateSession();
-        PageParser.userAgent = "Heliosbot/0.1 (+https://github.com/eseoa)";
-        Optional<Site> mainSite = session.createQuery("FROM Site WHERE url = :url ")
-                .setParameter("url", path)
-                .stream()
-                .findFirst();
-        if(mainSite.isPresent()) {
-            Site site = mainSite.get();
-            PageParser.parse(path, site);
-        }
-        else {
-            Transaction transaction = session.beginTransaction();
-            Site site = new Site(SiteStatus.INDEXED, LocalDateTime.now(), null, path, siteName);
-            session.save(site);
-            transaction.commit();
-            PageParser.parse(path, site);
-        }
-        Page page = (Page) session.createQuery("FROM Page WHERE path = :path")
-                .setParameter("path", path)
-                .stream()
-                .findFirst()
-                .get();
-        String expected = "200 https://skillbox.ru/";
-        String actual = page.getCode() + " " + page.getPath();
-        Assertions.assertEquals(expected, actual);
-    }
+//    private String path = "https://skillbox.ru/";
+//    private String siteName = "skillbox";
+//
+//    @Test
+//    @DisplayName("тест парсинга страницы")
+//    public void parse () {
+//        Session session = HibernateUtil.getHibernateSession();
+//        PageParser.userAgent = "Heliosbot/0.1 (+https://github.com/eseoa)";
+//        Optional<Site> mainSite = session.createQuery("FROM Site WHERE url = :url ")
+//                .setParameter("url", path)
+//                .stream()
+//                .findFirst();
+//        if(mainSite.isPresent()) {
+//            Site site = mainSite.get();
+//            PageParser.parse(path, site);
+//        }
+//        else {
+//            Transaction transaction = session.beginTransaction();
+//            Site site = new Site(SiteStatus.INDEXED, LocalDateTime.now(), null, path, siteName);
+//            session.save(site);
+//            transaction.commit();
+//            PageParser.parse(path, site);
+//        }
+//        Page page = (Page) session.createQuery("FROM Page WHERE path = :path")
+//                .setParameter("path", path)
+//                .stream()
+//                .findFirst()
+//                .get();
+//        String expected = "200 https://skillbox.ru/";
+//        String actual = page.getCode() + " " + page.getPath();
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -17,61 +17,61 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class SearchTest {
-    private String path = "https://skillbox.ru/";
-    private String siteName = "skillbox";
-
-    @BeforeEach
-    private void before() {
-        Session session = HibernateUtil.getHibernateSession();
-        PageParser.userAgent ="Heliosbot/0.1 (+https://github.com/eseoa)";
-        Optional<Site> mainSite = session.createQuery("FROM Site WHERE url = :url ")
-                .setParameter("url", path)
-                .stream()
-                .findFirst();
-        if(mainSite.isPresent()) {
-            Site site = mainSite.get();
-            PageParser.parse(path, site);
-            session.close();
-        }
-        else {
-            Transaction transaction = session.beginTransaction();
-            Site site = new Site(SiteStatus.INDEXED, LocalDateTime.now(), null, path, siteName);
-            session.save(site);
-            transaction.commit();
-            session.close();
-            PageParser.parse(path, site);
-        }
-    }
-
-    @Test
-    @DisplayName("поиск по всем сайтам")
-    public void fullSearch () {
-        Session session = HibernateUtil.getHibernateSession();
-        Page page = (Page) session.createQuery("FROM Page WHERE path = :path")
-                .setParameter("path", path)
-                .stream()
-                .findFirst()
-                .get();
-        session.close();
-        String expected = "skillbox";
-        Document document = Jsoup.parse(page.getContent());
-        String actual = Search.search(document.body().text()).get(0).getSiteName();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("поиск по конкретному сайту")
-    public void siteSearch () {
-        Session session = HibernateUtil.getHibernateSession();
-        Page page = (Page) session.createQuery("FROM Page WHERE path = :path")
-                .setParameter("path", path)
-                .stream()
-                .findFirst()
-                .get();
-        session.close();
-        String expected = "skillbox";
-        Document document = Jsoup.parse(page.getContent());
-        String actual = Search.search(document.body().text(), path).get(0).getSiteName();
-        Assertions.assertEquals(expected, actual);
-    }
+//    private String path = "https://skillbox.ru/";
+//    private String siteName = "skillbox";
+//
+//    @BeforeEach
+//    private void before() {
+//        Session session = HibernateUtil.getHibernateSession();
+//        PageParser.userAgent ="Heliosbot/0.1 (+https://github.com/eseoa)";
+//        Optional<Site> mainSite = session.createQuery("FROM Site WHERE url = :url ")
+//                .setParameter("url", path)
+//                .stream()
+//                .findFirst();
+//        if(mainSite.isPresent()) {
+//            Site site = mainSite.get();
+//            PageParser.parse(path, site);
+//            session.close();
+//        }
+//        else {
+//            Transaction transaction = session.beginTransaction();
+//            Site site = new Site(SiteStatus.INDEXED, LocalDateTime.now(), null, path, siteName);
+//            session.save(site);
+//            transaction.commit();
+//            session.close();
+//            PageParser.parse(path, site);
+//        }
+//    }
+//
+//    @Test
+//    @DisplayName("поиск по всем сайтам")
+//    public void fullSearch () {
+//        Session session = HibernateUtil.getHibernateSession();
+//        Page page = (Page) session.createQuery("FROM Page WHERE path = :path")
+//                .setParameter("path", path)
+//                .stream()
+//                .findFirst()
+//                .get();
+//        session.close();
+//        String expected = "skillbox";
+//        Document document = Jsoup.parse(page.getContent());
+//        String actual = Search.search(document.body().text()).get(0).getSiteName();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    @DisplayName("поиск по конкретному сайту")
+//    public void siteSearch () {
+//        Session session = HibernateUtil.getHibernateSession();
+//        Page page = (Page) session.createQuery("FROM Page WHERE path = :path")
+//                .setParameter("path", path)
+//                .stream()
+//                .findFirst()
+//                .get();
+//        session.close();
+//        String expected = "skillbox";
+//        Document document = Jsoup.parse(page.getContent());
+//        String actual = Search.search(document.body().text(), path).get(0).getSiteName();
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
